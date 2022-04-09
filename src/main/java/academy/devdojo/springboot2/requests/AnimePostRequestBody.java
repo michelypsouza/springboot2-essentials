b03eb016de8698fd9fffe5074a1090d17b8e200e
michelypsouza <michely.psouza@gmail.com>
2022-04-09 03:47:53 -0300

    aula 20 do curso de spring boot 2 essentials da devdojo academy

--- src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
+++ src/main/java/academy/devdojo/springboot2/requests/AnimePostRequestBody.java
@@ -2,7 +2,13 @@ package academy.devdojo.springboot2.requests;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotEmpty;
+
 @Data
 public class AnimePostRequestBody {
+
+    @NotEmpty(message = "The anime name cannot be empty")
+//    @NotNull(message = "The anime name cannot be null") // a validacao acima ja faz isso tbm
     private String name;
+
 }
